fa00ca4e72d1cf0f79370c4b126ccc36af0f83b3
Kairzhan Aubekerov <kairzhanable@gmail.com>
2020-03-04 17:28:27 +0300

    Reverted back to stable

--- app/src/main/java/ru/spbstu/quiz/MainActivity.java
+++ app/src/main/java/ru/spbstu/quiz/MainActivity.java
@@ -2,12 +2,11 @@ package ru.spbstu.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     Button btnNext;
 
@@ -17,32 +16,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btnNext = findViewById(R.id.buttonNext);
-        btnNext.setOnClickListener(this);
-
-
-        /*
-        View.OnClickListener btnNext = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-
-            }
-        };
-        */
 
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.buttonNext:
-                // TODO Call second activity
-                Intent intent = new Intent(this, ActivityTwo.class);
-                startActivity(intent);
-                break;
-            default:
-                break;
-            }
-        }
-    }
-
+}
